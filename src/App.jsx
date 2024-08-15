cb05cdbd8311c22b181bdfe9d750a523a49795b0
Peter Olesen <63056450+peter-olesen@users.noreply.github.com>
2024-08-15 10:54:31 +0200

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-// import { Header } from './components/Header/Header'
+import { Header } from './components/Header/Header'
 import { Main } from './components/Main/Main'
 import { Footer } from './components/Footer/Footer'
 import './App.scss'
@@ -7,7 +7,7 @@ function App() {
 
   return (
     <>
-      {/* <Header /> */}
+      <Header />
       <Main />
       <Footer />
     </>
